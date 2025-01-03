d95370e8016c78d11506ff717c9d1d702c1d0073
roaa-mamdouh <roaaabdelhadyy@gmail.com>
2025-01-03 11:47:33 +0200

    Beta

---
+++ crm/public/frontend/assets/Dashboard-fe4b2076.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./LayoutHeader-b53dcb1e.js";import{b as t,g as r,w as s,d as o,u as _}from"./index-cf070e44.js";import{_ as m}from"./Breadcrumbs.vue_vue_type_script_setup_true_lang-0218511c.js";import"./Dropdown.vue_vue_type_script_setup_true_lang-385f5ae2.js";const b={__name:"Dashboard",setup(c){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(i,l)=>(t(),r(e,null,{"left-header":s(()=>[o(_(m),{items:a})]),_:1}))}};export{b as default};
+//# sourceMappingURL=Dashboard-fe4b2076.js.map
